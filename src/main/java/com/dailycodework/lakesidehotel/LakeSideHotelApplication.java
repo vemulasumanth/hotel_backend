19f681e3d2c79eef89b2492d9992164a532118c2
vemulasumanth <vemulasumanth140500@gmail.com>
2024-05-28 17:12:37 +0530

    Describe your changes

--- src/main/java/com/dailycodework/lakesidehotel/LakeSideHotelApplication.java
+++ src/main/java/com/dailycodework/lakesidehotel/LakeSideHotelApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LakeSideHotelApplication {
 
+	public String PORT=System.getenv("PORT");
     public static void main(String[] args) {
         SpringApplication.run(LakeSideHotelApplication.class, args);
     }
